843b144ebfa399b7bf8cef25fd11b8932a96f99e
z <z@745>
2024-06-08 03:56:15 +0200

    Initial
    Initial

---
+++ src/ARMtest.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : ARMtest.cpp
+// Author      : z
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hey! This executes on ARM." << endl;
+
+	return 0;
+}
