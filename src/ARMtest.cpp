fb077aa5c362bdfca6c6a1f02bc9e7e973253e3a
zEclipseVM <60575976+nbgsmk@users.noreply.github.com>
2024-06-09 00:57:55 +0200

    Eclipse VM

--- src/ARMtest.cpp
+++ src/ARMtest.cpp
@@ -7,10 +7,13 @@
 //============================================================================
 
 #include <iostream>
+//#include <wiringPi.h>
 using namespace std;
 
 int main() {
 	cout << "Hey! This executes on ARM." << endl;
+	int a = 2;
+	a++;
 
 	return 0;
 }
